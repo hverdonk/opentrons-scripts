86572e51ac5e24dd5e7e67c03ba9a269aa267329
Hannah Verdonk <hverdonk@berkeley.edu>
2019-09-05 14:29:11 -0700

    finished part 1 of mag-bind protocol (pending testing), began part 2

--- Mag-Bind_PlasmidDNA_96Kit_Part1.py
+++ Mag-Bind_PlasmidDNA_96Kit_Part1.py
@@ -1,8 +1,6 @@
 # Written by Hannah Verdonk
 # 4 Sept 2019
-from opentrons import labware, instruments, robot, modules
-
-# TODO: remember to split this into multiple scripts, based on when you run out of tips & need to replace them
+from opentrons import labware, instruments, modules
 
 # metadata
 metadata = {
@@ -16,10 +14,9 @@ samples = labware.load("corning_96_wellplate_360ul_flat", 8)
 mag_plate = labware.load('usascientific_96_wellplate_2.4ml_deep', 7, share=True)
 
 # Define reagents
-# TODO: a tuberack won't hold enough volume. change this to some kind of trough
-reagents = labware.load('opentrons-tuberack-2ml-eppendorf', 4)
-'''6 columns A-F, 4 rows (I think)
-Let Solution I be in A1, Solution II be in B1, N3 buffer in C1, 
+reagents = labware.load('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 4)
+'''
+Let Solution I be in A 50ML falcon tube in C1, Solution II be in D1, N3 buffer in C2
 '''
 
 # Define tip racks
@@ -30,7 +27,7 @@ tiprack_2 = labware.load('opentrons-tiprack-300ul', 11)
 magdeck = modules.load('magdeck', 7)
 
 # pipettes
-p300 = instruments.P300_Multi(
+p300 = instruments.P300_Single(
     mount='right',
     tip_racks=[tiprack_1, tiprack_2])
 
@@ -40,16 +37,16 @@ p300 = instruments.P300_Multi(
 # add Solution I/RNase A to each well & resuspend cells (24 mL total)
 p300.distribute(
     250,
-    reagents.wells('A1'),
+    reagents.wells('C1'),
     samples,
-    mix_after=(3, 50),
+    mix_after=(10, 50),
     new_tip='always'
 )
 
 # add Solution II to each well (24 mL total)
 p300.distribute(
     250,
-    reagents.wells('B1'),
+    reagents.wells('D1'),
     samples
 )
 
