27dae84284a038dee9348e3f7eb54f0fcef682e1
Hannah Verdonk <hverdonk@berkeley.edu>
2019-09-06 11:38:51 -0700

    Updates to TENS miniprep part 2, waiting on Joey Riepsaame for clarification on protocol

--- TENS_miniprep_Part2.py
+++ TENS_miniprep_Part2.py
@@ -1,4 +1,4 @@
-from opentrons import labware, instruments, robot
+from opentrons import labware, instruments, modules, robot
 
 # metadata
 metadata = {
@@ -9,13 +9,13 @@ metadata = {
 }
 
 # Define sample labware
+old_samples = labware.load("corning_96_wellplate_360ul_flat", 6)
 samples = labware.load("corning_96_wellplate_360ul_flat", 5)
 trash = labware.load('axygen_1_reservoir_90ml', 1)
 
 # Define reagents
-LB = labware.load('usascientific_12_reservoir_22ml', 9)
-TENS = labware.load('usascientific_12_reservoir_22ml', 4)
-NaAC = labware.load('usascientific_12_reservoir_22ml', 6)
+EtOH_100 = labware.load('usascientific_12_reservoir_22ml', 4, share='true')
+EtOH_70 = labware.load('usascientific_12_reservoir_22ml', 9)
 # TROUGHS MUST BE CALIBRATED BEFORE USE
 
 # Define tip racks
@@ -23,6 +23,9 @@ tiprack_1 = labware.load('opentrons-tiprack-300ul', 7)
 tiprack_2 = labware.load('opentrons-tiprack-300ul', 8)
 tiprack_3 = labware.load('opentrons-tiprack-300ul', 10)
 tiprack_4 = labware.load('opentrons-tiprack-300ul', 11)
+
+# Define module
+tempdeck = modules.load('tempdeck', 4)
 
 
 # pipette
@@ -33,37 +36,23 @@ p300 = instruments.P300_Multi(
 
 # Protocol:
 
-# Remove & discard supernatant after centrifuging
-p300.consolidate(
-    1200,
+# Transfer supernatant to a new plate. Tip remains 2 mm from the bottom of the well
+# to avoid disturbing the pellet
+p300.transfer(
+    450,
+    old_samples,
     samples,
-    trash,
     new_tip='always'
-)
+).bottom(2)
 
-# Resuspend either in 50 μL of LB or P1 buffer or TE/RNAse
+# Add 1 mL 100% ice cold EtOH
 p300.distribute(
-    50,
-    LB,
+    1000,
+    EtOH_100,
     samples,
     pipette_after=(10, 300),
     new_tip='always'
 )
 
-# Add TENS buffer
-p300.distribute(
-    300,
-    TENS,
-    samples,
-    pipette_after=(10, 300),
-    new_tip='always'
-)
-
-# Add NaAC
-p300.distribute(
-    100,
-    NaAC,
-    samples,
-    pipette_after=(10, 300),
-    new_tip='always'
-)
+# Spin down plate, then return to robot
+robot.pause()
