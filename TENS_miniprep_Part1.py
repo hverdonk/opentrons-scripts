f0abf71bc1931f3164fb7239ed0b7ed5be7d9616
Hannah Verdonk <hverdonk@berkeley.edu>
2019-09-06 16:12:09 -0700

    Fixed supernatant removal step to use the correct amount of tips

--- TENS_miniprep_Part1.py
+++ TENS_miniprep_Part1.py
@@ -30,16 +30,39 @@ p300 = instruments.P300_Multi(
     mount='right',
     tip_racks=[tiprack_1, tiprack_2, tiprack_3, tiprack_4])
 
+# Experiment parameters
+# input volume (in uL) in each well of deepwell plate
+well_volume = 1200
+
 
 # Protocol:
 
 # Remove & discard supernatant after centrifuging
-p300.consolidate(
-    1200,
-    samples,
-    trash,
-    new_tip='always'
-)
+
+# Dispense slowly to avoid splashes & contamination
+p300.set_flow_rate(dispense=100)  # in uL/s
+
+# Find number of transfers per well
+full_transfers = well_volume // 300
+remainder = (well_volume/300) - full_transfers
+partial_transfer_volume = round(300*remainder)
+
+# Discard full supernatant volume in 300 uL increments
+for c in range(len(samples.columns())):
+    p300.pick_up_tip(tiprack_1.columns(c))
+    n = full_transfers
+    while n > 0:
+        p300.aspirate(300, samples.columns(c))
+        p300.dispense(300, trash).top()
+        n -= 1
+    if partial_transfer_volume > 0:
+        p300.aspirate(partial_transfer_volume, samples.columns(c))
+        p300.dispense(partial_transfer_volume, trash).top()
+    p300.drop_tip()
+
+# Set dispense rate back to the default
+p300.set_flow_rate(dispense=300)
+
 
 # Resuspend either in 50 μL of LB or P1 buffer or TE/RNAse
 p300.distribute(
