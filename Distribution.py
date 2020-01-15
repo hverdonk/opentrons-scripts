3259246890ebb01a80f292716e37b4bead678bb3
Hannah Verdonk <hverdonk@berkeley.edu>
2020-01-15 11:01:12 -0800

    Updated the commands governing the distribution.

--- Distribution.py
+++ Distribution.py
@@ -8,6 +8,7 @@ metadata = {
     'description': 'Another simple protocol to get started using OT2',
 }
 
+
 def run(protocol_context):
     # Labware Setup
     tiprack = labware.load('opentrons_96_tiprack_300ul', '11')
@@ -23,18 +24,23 @@ def run(protocol_context):
     plate9 = labware.load("corning_96_wellplate_360ul_flat", '9')
 
     # Pipette Setup
-    p300 = protocol_context.load_instrument('p300_multi', 'right',
-                                            tip_racks=tiprack)
+    p300 = protocol_context.load_instrument('p300_multi', 'right', tip_racks=tiprack)
 
-    # commands
-    plate_list = [plate1, plate2, plate3, plate4, plate5, plate6, plate7, plate8, plate9]
+    plate_list_1 = [plate1, plate2, plate3, plate4, plate5, plate6]
+    plate_list_2 = [plate7, plate8, plate9]
 
-    for p in plate_list:
-        pipette.distribute(
-            50,
-            master_plate,
-            p,
-            mix_before=(3, 100),
-            disposal_vol=10
-        )   # include extra liquid "disposal_vol" to make dispenses more accurate
+    # Distribute volume from deep well plate to flat bottom plates, column by column
+    for col in master_plate.columns_by_name():
+        p300.pick_up_tip()
+        p300.aspirate(275, master_plate[col])
+        p300.dispense(45, [plate.columns_by_name()[col] for plate in plate_list_1])
+        p300.aspirate(135, master_plate[col])
+        p300.dispense(45, [plate.columns_by_name()[col] for plate in plate_list_2])
+        p300.drop_tip()
 
+    # from master_plate, aspirate 275uL from column 1
+    # Dispense 45uL into column 1 of plates 1-6 (270uL total)
+    # from master_plate, aspirate 135uL from column 1 (140uL total)
+    # dispense 45uL into column 1 of plates 7-9 (135uL total)
+    # drop tips
+    # repeat with other columns
