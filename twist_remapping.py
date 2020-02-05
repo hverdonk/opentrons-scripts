06f0f1f52b828d7433d7ea87e0234958089ae485
Hannah Verdonk <hverdonk@berkeley.edu>
2020-02-05 15:53:47 -0800

    Completed m_pneumonia_twist_remap_0.py

--- twist_remapping.py
+++ twist_remapping.py
@@ -5,48 +5,41 @@ json1_str = json1_file.read()
 json1_data = json.loads(json1_str)
 json1_file.close()
 
-# Uncomment to find new plate names
-print(json1_data.keys())
-
 '''
 Keeping these notes for later code improvements.
 
 json1_data is a list of dicts. Each dict has key = old plate name, value = list of dicts 
 governing moves to make.
 json1_data['M.gentalium_1']
-
-take len of this to find out how many plates to take FROM
-len(json1_data['M.gentalium_1'])
-
-8 plates used to build 'M.gentalium_0'
-7 plates used to build 'M.gentalium_1'
-
-Find plate names & the order in which they appear:
-for plate in json1_data['M.gentalium_0']:
-    print(plate)
-
-
-Calculating # of tips used (aka number of move operations)
-Find the number of dicts per old plate. Each dict is a move, and each move 
-requires a new tip to avoid contamination.
-
-moves = 0
-for plate in json1_data['M.gentalium_1']:
-    moves = moves + len(plate)
-
-print("moves = {}".format(moves))
-
-Creating plate 1 will require 21 tips - one box will suffice.
 '''
+
+# Uncomment to find new plate names.
+# print(json1_data.keys())
+
+new_plate = 'M.pneumonia_0'
+
+# Uncomment to find old plate names, and how many there are.
+# print("Number of old plates: {}".format(len(json1_data[new_plate])))
+# for plate in json1_data[new_plate]:
+#     print(plate)
+
+# Uncomment to calculate # of tips used (aka number of move operations)
+# Find the number of dicts per old plate. Each dict is a move, and each move
+# requires a new tip to avoid contamination.
+
+# moves = 0
+# for plate in json1_data[new_plate]:
+#     moves = moves + len(json1_data[new_plate][plate])
+#
+# print("moves = {}".format(moves))
+
 
 t = open('transfers.txt','w')
 
 # Extract source and destination wells from json, then write them to new file for transfer.
-
-'''
-for plate in json1_data['M.gentalium_1']:
-    p_name = "plate_" + plate
-    for move in json1_data['M.gentalium_1'][plate]:
+for p in json1_data[new_plate]:
+    p_name = "plate_" + p
+    for move in json1_data[new_plate][p]:
         source_well = move['from']['address']
         dest_well = move['to']['address']
         command = "p10.transfer(5, " \
@@ -57,8 +50,5 @@ for plate in json1_data['M.gentalium_1']:
                   + dest_well \
                   + "\"])" + "\n"
         t.write(command)
-
 t.close()
-
-'''
 
