c979f1122bc3c0c4d913c49c51ece1ccbb5299cf
Hannah Verdonk <hverdonk@berkeley.edu>
2020-02-04 16:46:19 -0800

    Took some notes on json file that Keoni provided to govern robot movement.

--- twist_remapping.py
+++ twist_remapping.py
@@ -1,10 +1,46 @@
 from opentrons import protocol_api,labware, instruments
+import json
 
 metadata = {
 
     'protocolName': 'twist_remapping',
     'author': 'Hannah Verdonk <hverdonk@stanford.edu>',
     'apiLevel':'2.1'}
+
+
+json1_file = open('data.json', 'r')
+json1_str = json1_file.read()
+json1_data = json.loads(json1_str)
+json1_file.close()
+
+print('keys')  # Each key is a new plate to be filled
+print(json1_data.keys())
+
+print("values")
+print(len(json1_data['M.gentalium_1']))
+
+
+'''
+a list of dicts. each dict has key = old plate name, value = list of dicts 
+governing moves to make.
+json1_data['M.gentalium_1']
+
+take len of this to find out how many plates to take FROM
+len(json1_data['M.gentalium_1'])
+
+8 plates used to build 'M.gentalium_0'
+7 plates used to build 'M.gentalium_1'
+
+
+Calculating # of tips used (aka number of move operations)
+Find the number of dicts per old plate. Each dict is a move, and each move 
+requires a new tip to avoid contamination.
+
+'''
+moves = 0
+ for _ in _:
+
+
 
 
 def run(protocol_context):
@@ -20,12 +56,3 @@ def run(protocol_context):
     # instrument setup
     p10 = protocol_context.load_instrument('p10_single', 'left', tip_racks=tipracks_10ul)
     p50 = protocol_context.load_instrument('p50_single', 'right', tip_racks=tiprack_300ul)
-
-
-    # reagent setup
-
-    mastermix = rt_reagents.columns()[0][0]
-    primer_1 = rt_reagents.columns()[1][0]
-    primer_2 = rt_reagents.columns()[1][1]
-    water = rt_reagents.columns()[2][0]
-    dna = rt_reagents.columns()[3][0]
