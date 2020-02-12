b5934186589af94be42ee8d8addbc2ff2f39baab
hverdonk <31907124+hverdonk@users.noreply.github.com>
2020-02-12 11:33:00 -0800

    quick fix to make_distros

--- make_distros_50ul_Multi.py
+++ make_distros_50ul_Multi.py
@@ -24,15 +24,17 @@ def run(protocol_context):
     plate9 = protocol_context.load_labware("corning_96_wellplate_360ul_flat", '9')
 
     # Pipette Setup
-    p300 = protocol_context.load_instrument('p300_multi', 'left', tip_racks=[p300rack])
-    p300.flow_rate.blow_out = 400   # blow out faster to avoid droplets clinging to pipette tips
+    p50 = protocol_context.load_instrument('p50_multi', 'left', tip_racks=[p300rack])
+    p50.flow_rate.blow_out = 400   # blow out faster to avoid droplets clinging to pipette tips
 
     plate_list = [plate1, plate2, plate3, plate4, plate5, plate6, plate7, plate8, plate9]
 
+
     # Distribute volume from deep well plate to flat bottom plates, column by column
+    # Move right to left (i.e., start at column 12) to avoid contaminating virgin wells
     # Only officially take from well 0 in the column, because the multichannel will reach the rest anyway
     right = len(master_plate.columns()) - 1
     left = -1
     for col in range(right, left, -1):
-        p300.distribute(45, master_plate.columns()[col][0],
+        p50.distribute(45, master_plate.columns()[col][0],
                            [plate.columns()[col][0] for plate in plate_list])
