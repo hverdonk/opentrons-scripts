1b63ffd955648192016993124bcc10b475e09a6f
Hannah Verdonk <hverdonk@berkeley.edu>
2020-02-20 14:25:27 -0800

    Made B. subtilis Twist plate remapping protocol.

--- make_twist_remap.py
+++ make_twist_remap.py
@@ -1,7 +1,7 @@
 import json
 
 # change as necessary for different distros
-json1_file = open('mgenitalium.json', 'r')
+json1_file = open('bsubtilis.json', 'r')
 json1_str = json1_file.read()
 json1_data = json.loads(json1_str)
 json1_file.close()
@@ -15,9 +15,10 @@ json1_data['M.genitalium_1']
 '''
 
 # Uncomment to find new plate names.
+# print("New Plate Names: ")
 # print(json1_data.keys())
 
-new_plate = 'M.genitalium_1'
+new_plate = 'B.subtilis_2'
 
 # Uncomment to find old plate names, and how many there are.
 # Could theoretically exceed what can fit on the deck, but in practice doesn't actually happen
