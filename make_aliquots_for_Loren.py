65ebe6b6b86c36a1880bd0a3afa8c0bb5f867317
hverdonk <31907124+hverdonk@users.noreply.github.com>
2020-06-10 12:43:12 -0700

    changed pipette from p50 to p10

--- make_aliquots_for_Loren.py
+++ make_aliquots_for_Loren.py
@@ -22,7 +22,7 @@ def run(protocol: protocol_api.ProtocolContext):
     """
 
     # Labware Setup
-    p50rack = protocol.load_labware('opentrons_96_filtertiprack_200ul', '11')
+    p10rack = protocol.load_labware('geb_96_tiprack_10ul', '11')
     destination = protocol.load_labware("biorad_96_wellplate_200ul_pcr", '10')
 
     # Source Twist plates
@@ -35,51 +35,51 @@ def run(protocol: protocol_api.ProtocolContext):
     plate7 = protocol.load_labware("corning_96_wellplate_360ul_flat", '7')
     plate8 = protocol.load_labware("corning_96_wellplate_360ul_flat", '8')
 
-    p50 = protocol.load_instrument('p50_single', 'right', tip_racks=[p50rack])
+    p10 = protocol.load_instrument('p10_single', 'right', tip_racks=[p10rack])
 
     # BEGIN PROTOCOL
     # plate1 transfers
-    p50.transfer(5, plate1.wells_by_name()['F10'], destination.wells_by_name()['A1'])
-    p50.transfer(5, plate1.wells_by_name()['F11'], destination.wells_by_name()['B1'])
-    p50.transfer(5, plate1.wells_by_name()['E7'], destination.wells_by_name()['C1'])
-    p50.transfer(5, plate1.wells_by_name()['F2'], destination.wells_by_name()['D1'])
-    p50.transfer(5, plate1.wells_by_name()['F9'], destination.wells_by_name()['E1'])
+    p10.transfer(5, plate1.wells_by_name()['F10'], destination.wells_by_name()['A1'])
+    p10.transfer(5, plate1.wells_by_name()['F11'], destination.wells_by_name()['B1'])
+    p10.transfer(5, plate1.wells_by_name()['E7'], destination.wells_by_name()['C1'])
+    p10.transfer(5, plate1.wells_by_name()['F2'], destination.wells_by_name()['D1'])
+    p10.transfer(5, plate1.wells_by_name()['F9'], destination.wells_by_name()['E1'])
 
     # plate2 transfers
-    p50.transfer(5, plate2.wells_by_name()['A9'], destination.wells_by_name()['F1'])
-    p50.transfer(5, plate2.wells_by_name()['G8'], destination.wells_by_name()['G1'])
-    p50.transfer(5, plate2.wells_by_name()['H8'], destination.wells_by_name()['H1'])
-    p50.transfer(5, plate2.wells_by_name()['H3'], destination.wells_by_name()['A2'])
-    p50.transfer(5, plate2.wells_by_name()['B2'], destination.wells_by_name()['B2'])
+    p10.transfer(5, plate2.wells_by_name()['A9'], destination.wells_by_name()['F1'])
+    p10.transfer(5, plate2.wells_by_name()['G8'], destination.wells_by_name()['G1'])
+    p10.transfer(5, plate2.wells_by_name()['H8'], destination.wells_by_name()['H1'])
+    p10.transfer(5, plate2.wells_by_name()['H3'], destination.wells_by_name()['A2'])
+    p10.transfer(5, plate2.wells_by_name()['B2'], destination.wells_by_name()['B2'])
 
     # plate3 transfers
-    p50.transfer(5, plate3.wells_by_name()['D5'], destination.wells_by_name()['C2'])
-    p50.transfer(5, plate3.wells_by_name()['C2'], destination.wells_by_name()['D2'])
-    p50.transfer(5, plate3.wells_by_name()['B3'], destination.wells_by_name()['E2'])
-    p50.transfer(5, plate3.wells_by_name()['H3'], destination.wells_by_name()['F2'])
-    p50.transfer(5, plate3.wells_by_name()['F3'], destination.wells_by_name()['G2'])
+    p10.transfer(5, plate3.wells_by_name()['D5'], destination.wells_by_name()['C2'])
+    p10.transfer(5, plate3.wells_by_name()['C2'], destination.wells_by_name()['D2'])
+    p10.transfer(5, plate3.wells_by_name()['B3'], destination.wells_by_name()['E2'])
+    p10.transfer(5, plate3.wells_by_name()['H3'], destination.wells_by_name()['F2'])
+    p10.transfer(5, plate3.wells_by_name()['F3'], destination.wells_by_name()['G2'])
 
     # plate4 transfers
-    p50.transfer(5, plate4.wells_by_name()['H3'], destination.wells_by_name()['H2'])
-    p50.transfer(5, plate4.wells_by_name()['H7'], destination.wells_by_name()['A3'])
-    p50.transfer(5, plate4.wells_by_name()['F12'], destination.wells_by_name()['B3'])
-    p50.transfer(5, plate4.wells_by_name()['C9'], destination.wells_by_name()['C3'])
+    p10.transfer(5, plate4.wells_by_name()['H3'], destination.wells_by_name()['H2'])
+    p10.transfer(5, plate4.wells_by_name()['H7'], destination.wells_by_name()['A3'])
+    p10.transfer(5, plate4.wells_by_name()['F12'], destination.wells_by_name()['B3'])
+    p10.transfer(5, plate4.wells_by_name()['C9'], destination.wells_by_name()['C3'])
 
     # plate5 transfers
-    p50.transfer(5, plate5.wells_by_name()['H6'], destination.wells_by_name()['D3'])
-    p50.transfer(5, plate5.wells_by_name()['D5'], destination.wells_by_name()['E3'])
-    p50.transfer(5, plate5.wells_by_name()['D9'], destination.wells_by_name()['F3'])
+    p10.transfer(5, plate5.wells_by_name()['H6'], destination.wells_by_name()['D3'])
+    p10.transfer(5, plate5.wells_by_name()['D5'], destination.wells_by_name()['E3'])
+    p10.transfer(5, plate5.wells_by_name()['D9'], destination.wells_by_name()['F3'])
 
     # plate6 transfers
-    p50.transfer(5, plate6.wells_by_name()['B2'], destination.wells_by_name()['G3'])
-    p50.transfer(5, plate6.wells_by_name()['F8'], destination.wells_by_name()['H3'])
-    p50.transfer(5, plate6.wells_by_name()['B3'], destination.wells_by_name()['A4'])
+    p10.transfer(5, plate6.wells_by_name()['B2'], destination.wells_by_name()['G3'])
+    p10.transfer(5, plate6.wells_by_name()['F8'], destination.wells_by_name()['H3'])
+    p10.transfer(5, plate6.wells_by_name()['B3'], destination.wells_by_name()['A4'])
 
     # plate7 transfers
-    p50.transfer(5, plate7.wells_by_name()['H6'], destination.wells_by_name()['B4'])
-    p50.transfer(5, plate7.wells_by_name()['D12'], destination.wells_by_name()['C4'])
+    p10.transfer(5, plate7.wells_by_name()['H6'], destination.wells_by_name()['B4'])
+    p10.transfer(5, plate7.wells_by_name()['D12'], destination.wells_by_name()['C4'])
 
     # plate8 transfers
-    p50.transfer(5, plate8.wells_by_name()['G6'], destination.wells_by_name()['D4'])
-    p50.transfer(5, plate8.wells_by_name()['E5'], destination.wells_by_name()['E4'])
-    p50.transfer(5, plate8.wells_by_name()['E3'], destination.wells_by_name()['F4'])
+    p10.transfer(5, plate8.wells_by_name()['G6'], destination.wells_by_name()['D4'])
+    p10.transfer(5, plate8.wells_by_name()['E5'], destination.wells_by_name()['E4'])
+    p10.transfer(5, plate8.wells_by_name()['E3'], destination.wells_by_name()['F4'])
